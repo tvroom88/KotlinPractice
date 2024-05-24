f51835011934a95125131a4d9224b1bb0248b19d
tvroom9102 <tvroom9102@gmail.com>
2024-05-24 15:45:49 +0900

    object 설명 및 예제 추가. 1. 객체 선언(Object Declaration) 2. 동반 객체 (Companion Object) 3. 객체 표현식 (Object Expression)
    Object Expression 예제 및 내용 추가 필요!

--- app/src/main/java/com/example/kotlinpractice/class_example/object_singleton/ObjectSingleton.kt
+++ app/src/main/java/com/example/kotlinpractice/class_example/object_singleton/ObjectSingleton.kt
@@ -22,6 +22,7 @@ fun main() {
 
 }
 
+// 1. 객체 선언(Object Declaration)
 object ObjectCounter {
     var count = 0
     fun countUp() {
@@ -29,14 +30,44 @@ object ObjectCounter {
     }
 }
 
+// 2. 동반 객체 (Companion Object)
 class ObjectFoodPoll(val name: String) {
     companion object {
         var total = 0
     }
+
     var count = 0
     fun vote() {
         total++
         count++
     }
+}
+
+//3. 객체 표현식 (Object Expression)
+// 참고 url : https://rnathsus.tistory.com/335
+class ObjectExpression() {
+
+    // 처음부터 익명 객체 생성하기
+    // 객체 표현식의 시작은 object 키워드로 시작합니다. 부모 타입을 가지지 않는 단순 익명 객체를 만들고자 한다면, 아래 코드와 같이 작성합니다.
+    val helloKO = object {
+        val message = "안녕"
+        override fun toString(): String {
+            return message
+        }
+    }
+
+    // 부모 타입으로 부터 상속 받는 익명 객체 생성하기
+    // 익명 객체가 상속을 받는다고 생각하면 된다.
+    val helloEn = object : SayHi() {
+        override fun toString(): String {
+            return message
+        }
+    }
+
+    // 만약에 부모의 생성자를 가지고 있으면, 적절한 생성자 파라미터를 넘겨줄 수 있습니다.
+}
+
+open class SayHi {
+    open val message = "hello"
 }
 
